8edb6f9cf40ede27c650614aec05ad8ccebc23bd
Dheerajvavilala <dheerajvavilala5@gmail.com>
2024-06-27 12:00:32 +0530

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     validates :name , presence: true,length:{maximum:50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
-    validates :password,presence: true, length: {minimum:6}
+    validates :password,presence: true, length: {minimum:6},allow_nil: true
     has_secure_password
     # Returns the hash digest of the given string.
     def User.digest(string)
@@ -20,7 +20,15 @@ class User < ApplicationRecord
     def remember
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
+        remember_digest
     end
+
+    # Returns a session token to prevent session hijacking.
+    # We reuse the remember digest for convenience.
+    def session_token
+        remember_digest || remember
+    end
+    
 
 
     # Returns true if the given token matches the digest.
